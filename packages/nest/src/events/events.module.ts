b772d6026cb047becce25bef5cc1910c0d054eb7
JASON SUAREZ <suarezjason@icloud.com>
2025-02-09 18:33:11 +0100

    refact: update event service to handle watching events from pairs created

--- packages/nest/src/events/events.module.ts
+++ packages/nest/src/events/events.module.ts
@@ -3,10 +3,11 @@ import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { eventProviders } from './event.providers';
 import { DatabaseModule } from 'src/config/database.module';
+import { poolProviders } from 'src/pools/pool.providers';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [...eventProviders, EventsService],
+  providers: [...eventProviders, ...poolProviders, EventsService],
   controllers: [EventsController],
 })
 export class EventsModule {}
--- packages/nest/src/events/events.service.ts
+++ packages/nest/src/events/events.service.ts
@@ -1,24 +1,91 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { createPublicClient, http, parseAbiItem } from 'viem';
 import { Event, EventType } from './event.entity';
 import { REPOSITORIES } from 'src/utils/constants';
 import { config } from 'dotenv';
+import { Pool } from 'src/pools/pool.entity';
 config();
 @Injectable()
-export class EventsService {
+export class EventsService implements OnModuleInit {
   private client;
   private unwatch?: () => void;
 
   constructor(
     @Inject(REPOSITORIES.EVENT)
     private eventRepository: Repository<Event>,
+    @Inject(REPOSITORIES.POOL)
+    private poolsService: Repository<Pool>,
   ) {
     this.client = createPublicClient({
       transport: http(process.env.RPC_URL),
     });
   }
+  async onModuleInit() {
+    console.log('Starting event watching service...');
+    await this.startWatching();
+  }
 
+  private async startWatching() {
+    // First listen to Factory events
+    await this.watchFactoryEvents();
+  }
+
+  private async watchFactoryEvents() {
+    const factoryAddress = process.env.FACTORY_ADDRESS;
+
+    // Watch for new pools
+    await this.client.watchContractEvent({
+      address: factoryAddress,
+      abi: [
+        parseAbiItem(
+          'event PairCreated(address indexed token0,address indexed token1,address pair,uint)',
+        ),
+      ],
+      onLogs: async (logs) => {
+        for (const log of logs) {
+          // Save new pool to database
+          const poolAddress = log.args.pool;
+          const pool = this.poolsService.create({
+            address: poolAddress,
+            token0: log.args.tokenA,
+            token1: log.args.tokenB,
+          });
+
+          await this.poolsService.save(pool);
+
+          // Start watching this pool's events
+          await this.watchPoolEvents(poolAddress);
+        }
+      },
+    });
+  }
+
+  private async watchPoolEvents(poolAddress: string) {
+    console.log('Watching pool events for:', poolAddress);
+    await this.client.watchContractEvent({
+      address: poolAddress,
+      abi: [
+        parseAbiItem(
+          'event Mint(address indexed sender, uint amount0, uint amount1)',
+        ),
+        parseAbiItem(
+          'event Burn(address indexed sender, uint amount0, uint amount1, address indexed to)',
+        ),
+        parseAbiItem(
+          'event Swap(address indexed sender, uint amountIn, uint amountOut)',
+        ),
+        // ... other pool events
+      ],
+      onLogs: async (logs) => {
+        for (const log of logs) {
+          // await this.handlePoolEvent(log);
+
+          console.log('Received log:', log);
+        }
+      },
+    });
+  }
   async startListening(contractAddress: string) {
     try {
       console.log('Starting to listen to events...');
