d4cd8f4d4995ea6eec0df8a3b95483991076060e
Jason Suarez <suarezjason@icloud.com>
2025-02-12 09:09:39 +0100

    ui: add links to header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,6 +4,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Telescope, UserPen } from "lucide-react";
 import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
@@ -18,6 +19,16 @@ export const menuLinks: HeaderMenuLink[] = [
   {
     label: "Home",
     href: "/",
+  },
+  {
+    label: "Explorer",
+    href: "/explorer",
+    icon: <Telescope className="h-4 w-4" />,
+  },
+  {
+    label: "Providers",
+    href: "/details",
+    icon: <UserPen className="h-4 w-4" />,
   },
 
   {
