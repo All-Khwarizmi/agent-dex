b4589cf3b04f5d6b865b4ab6d250375676169cee
Jason Suarez <suarezjason@icloud.com>
2025-02-13 09:54:46 +0100

    refact: add variable and change type to be more explicit

--- packages/foundry/contracts/Factory.sol
+++ packages/foundry/contracts/Factory.sol
@@ -18,6 +18,11 @@ contract Factory is IFactory {
     address[] public allPairs;
     mapping(address => mapping(address => address)) public getPair;
 
+    /**
+     * Function to create a pair (pool)
+     * @param token0 address of the first token
+     * @param token1 address of the second token
+     */
     function createPair(
         address token0,
         address token1
@@ -34,16 +39,20 @@ contract Factory is IFactory {
             address(i_uniswapV2Factory),
             address(i_uniswapV2Router)
         );
-        getPair[token0][token1] = address(_pair);
-        getPair[token1][token0] = address(_pair);
-        allPairs.push(address(_pair));
+        address pairAddress = address(_pair);
+        getPair[token0][token1] = pairAddress;
+        getPair[token1][token0] = pairAddress;
+        allPairs.push(pairAddress);
 
-        emit PairCreated(token0, token1, address(_pair), allPairs.length);
+        emit PairCreated(token0, token1, pairAddress, allPairs.length);
 
-        return address(_pair);
+        return pairAddress;
     }
 
-    function getPairCount() public view returns (uint poolCount) {
+    /**
+     * Function to get the amount of pairs that have been created
+     */
+    function getPairCount() public view returns (uint256 poolCount) {
         poolCount = allPairs.length;
     }
 }
